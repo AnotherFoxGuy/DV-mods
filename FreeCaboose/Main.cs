7887372b07b2bf8d0f702a66a809b8f7237dd7af
Edgar <Edgar@AnotherFoxGuy.com>
2020-08-17 16:39:58 +0200

    :tada: Initial commit

---
+++ FreeCaboose/Main.cs
@@ -0,0 +1,27 @@
+﻿using DV;
+using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace FreeCaboose
+{
+    public class Main
+    {
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            return true;
+        }
+        
+        [HarmonyPatch(typeof(CommsRadioCrewVehicle), "SummonPrice", MethodType.Getter)]
+        static class FreeCaboose_Patch
+        {            
+            static float Postfix(float f)
+            {
+                return 0.0f;
+            }
+        }
+    }
+}
